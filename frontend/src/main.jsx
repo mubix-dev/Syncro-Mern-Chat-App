e5e2fbaf8d7f9f78a23616a57331e64a0736369b
Mubeen Khan <mubix.dev@gmail.com>
2026-04-28 18:17:00 +0500

    add backend url in serverURL variable

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom"
 import  {Provider} from "react-redux"
 import store from './redux/store.js'
 
-export const serverURL = "http://localhost:3000"
+export const serverURL = "https://syncro-mern-chat-app.vercel.app"
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <Provider store={store}>
