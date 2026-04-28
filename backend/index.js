a6cd1c3ff894fdec4484ad1cadeffe2eda9eb984
Mubeen Khan <mubix.dev@gmail.com>
2026-04-28 18:09:05 +0500

    Resolve connectDB error

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,40 @@
-import express  from "express"
+import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js"
+import connectDB from "./config/db.js";
 import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 import userRouter from "./routes/user.routes.js";
 import messageRouter from "./routes/message.routes.js";
-import {app,server} from "./socket/socket.js"
+import { app, server } from "./socket/socket.js";
 
+dotenv.config({ override: true });
+const PORT = process.env.PORT || 3000;
 
-dotenv.config({override:true});
-const PORT = process.env.PORT || 3000
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/message", messageRouter);
 
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
-app.use("/api/message",messageRouter)
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
 
-
-app.get('/',(req,res)=>{
-    res.send("Server is running!")
-})
-
-
-
-server.listen(PORT ,()=>{
-    connectDB()
-    console.log(`Server is running at http://localhost:${PORT}`)
-})
+connectDB()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MogoDB connection error!", err);
+    process.exit(1);
+  });
