b0cb91ae885976142eae2ca28eab598dd4f892bc
Mubeen Khan <mubix.dev@gmail.com>
2026-04-30 23:08:54 +0500

    add email-verify route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import {login, logout, signUp} from "../controllers/auth.controller.js"
+import {login, logout, signUp, verifyEmail} from "../controllers/auth.controller.js"
 
 const authRouter = express.Router();
 
 authRouter.post("/signup",signUp)
 authRouter.post("/login",login)
 authRouter.get("/logout",logout)
+authRouter.post("/verify-email",verifyEmail)
 
 export default authRouter;
