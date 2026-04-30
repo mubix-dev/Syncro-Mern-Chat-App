49eab27e72d6dd06740db08b3a3211d9eae978de
Mubeen Khan <mubix.dev@gmail.com>
2026-04-30 23:12:22 +0500

    Update routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { io } from "socket.io-client";
+
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import useCurrentUser from "./hooks/useCurrentUser.js";
-import { useDispatch, useSelector } from "react-redux";
 import Profile from "./pages/Profile.jsx";
 import Home from "./pages/Home.jsx";
+import VerifyEmail from "./pages/VerifyEmail.jsx"; 
+
+import useCurrentUser from "./hooks/useCurrentUser.js";
 import useOtherUserData from "./hooks/useOtherUsersData.js";
 import useGetMessages from "./hooks/useGetMessages.js";
-import { io } from "socket.io-client";
-import { useEffect } from "react";
 import { serverURL } from "./main.jsx";
 import { setOnlineUsers, setSocket } from "./redux/userSlice.js";
 
@@ -17,15 +19,16 @@ function App() {
   useCurrentUser();
   useOtherUserData();
   useGetMessages();
-  let { userData, socket, onlineUsers } = useSelector((state) => state.user);
-  let dispatch = useDispatch();
+  
+  const { userData, socket } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    if (userData) {
+    if (userData && userData.isVerified) {
       const socketIo = io(`${serverURL}`, {
-        query: {
-          userId: userData?._id,
-        },
+        query: { userId: userData._id },
       });
+
       dispatch(setSocket(socketIo));
 
       socketIo.on("getOnlineUsers", (users) => {
@@ -34,32 +37,30 @@ function App() {
 
       return () => {
         socketIo.close();
+        dispatch(setSocket(null));
       };
-    }else{
-      if(socket){
-        socket.close()
-      dispatch(setSocket(null))
-      }
+    } else if (socket) {
+      socket.close();
+      dispatch(setSocket(null));
     }
-  }, [userData]);
+  }, [userData]); 
+
   return (
     <Routes>
-      <Route
-        path="/login"
-        element={!userData ? <Login /> : <Navigate to={"/"} />}
+      <Route path="/login" element={!userData ? <Login /> : <Navigate to="/" />} />
+      <Route path="/signup" element={!userData ? <Signup /> : <Navigate to="/" />} />
+      <Route path="/verify-email" element={userData?.isVerified ? <Navigate to="/profile" /> : <VerifyEmail />} />
+
+      <Route 
+        path="/" 
+        element={userData?.isVerified ? <Home /> : <Navigate to={userData ? "/verify-email" : "/login"} />} 
       />
-      <Route
-        path="/signup"
-        element={!userData ? <Signup /> : <Navigate to={"/profile"} />}
+      <Route 
+        path="/profile" 
+        element={userData?.isVerified ? <Profile /> : <Navigate to={userData ? "/verify-email" : "/login"} />} 
       />
-      <Route
-        path="/"
-        element={userData ? <Home /> : <Navigate to={"/login"} />}
-      />
-      <Route
-        path="/profile"
-        element={userData ? <Profile /> : <Navigate to={"/signup"} />}
-      />
+
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
